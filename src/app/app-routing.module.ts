25df016ed9b0058ef884fd85b4fc84e794138e83
mrohmer <matthias.rohmer@immowelt.de>
2021-02-02 14:44:26 +0100

    docs: add sample pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from './app.component';
+import {Test1Component} from './pages/test1/test1.component';
+import {Test2Component} from './pages/test2/test2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'test1',
+    component: Test1Component
+  },
+  {
+    path: 'test2',
+    component: Test2Component,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'test1'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {PortalModule} from '../../projects/rohmer/ng-portal/src/public-api';
+import { Test1Component } from './pages/test1/test1.component';
+import { Test2Component } from './pages/test2/test2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Test1Component,
+    Test2Component
   ],
   imports: [
     BrowserModule,
